6060f5f47d897ac3da89bbfc6bfa3ade65cf0420
mc-tangb <511598230@qq.com>
2017-08-01 23:32:06 +0800

    cm

--- lesson1/list.py
+++ lesson1/list.py
@@ -37,6 +37,39 @@ print(sorted(arr, reverse=True))
 print(arr)
 print(len(arr))
 
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() + ', that was a great trick!')
+print(magicians[0:3])
+print(magicians[:1])
+print(magicians[1:])
+print(magicians[-2:])    
+magicians2 = magicians[:]
+magicians2[0].title()
+print(magicians)
+print(magicians2)
+
+    
+for i in range(1, 5):
+    print (i)
+    
+print(list(range(1, 6)))
+
+squares = []
+for value in range(1, 11):
+    square = value ** 2
+    squares.append(square)
+    
+print(squares)
+
+digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+print(min(digits))
+print(max(digits))
+print(sum(digits))
+    
+squares2 = [square**2 for square in range(1, 11)]
+print(squares2)
+
 
 
 
